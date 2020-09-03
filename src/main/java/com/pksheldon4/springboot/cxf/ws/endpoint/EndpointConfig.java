ac759064abaed3ac75d24f873e2feb9b0f5d4055
Preston Sheldon <pksiv@hotmail.com>
2020-09-03 14:12:15 -0400

    Fix in/out logging

--- src/main/java/com/pksheldon4/springboot/cxf/ws/endpoint/EndpointConfig.java
+++ src/main/java/com/pksheldon4/springboot/cxf/ws/endpoint/EndpointConfig.java
@@ -2,25 +2,44 @@ package com.pksheldon4.springboot.cxf.ws.endpoint;
 
 
 import org.apache.cxf.Bus;
+import org.apache.cxf.ext.logging.LoggingFeature;
 import org.apache.cxf.jaxws.EndpointImpl;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import javax.xml.ws.Endpoint;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 @Configuration
 public class EndpointConfig {
 
-    @Autowired
-    private Bus bus;
+    private Bus cxfBus;
+
+    public EndpointConfig(Bus bus) {
+        this.cxfBus = bus;
+    }
 
     @Bean
     public Endpoint endpoint() {
-        EndpointImpl endpoint =
-            new EndpointImpl(bus, new HelloWorldImpl());
+        EndpointImpl endpoint = new EndpointImpl(bus(), new HelloWorldImpl());
         endpoint.publish("/helloworld");
-
         return endpoint;
+    }
+
+    @Bean
+    public Bus bus() {
+        cxfBus.setFeatures(
+            new ArrayList<>(Arrays.asList(loggingFeature())));
+
+        return cxfBus;
+    }
+
+    @Bean
+    public LoggingFeature loggingFeature() {
+        LoggingFeature loggingFeature = new LoggingFeature();
+        loggingFeature.setPrettyLogging(true);
+
+        return loggingFeature;
     }
 }
--- src/main/java/com/pksheldon4/springboot/cxf/ws/endpoint/HelloWorldImpl.java
+++ src/main/java/com/pksheldon4/springboot/cxf/ws/endpoint/HelloWorldImpl.java
@@ -5,13 +5,16 @@ import com.pksheldon4.services.helloworld.HelloWorldPortType;
 import com.pksheldon4.types.helloworld.Greeting;
 import com.pksheldon4.types.helloworld.ObjectFactory;
 import com.pksheldon4.types.helloworld.Person;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 public class HelloWorldImpl implements HelloWorldPortType {
 
     @Override
     public Greeting sayHello(Person request) {
+        log.warn("############################### sayHello");
         String greeting = "Hello " + request.getFirstName() + " "
-                + request.getLastName() + "!";
+            + request.getLastName() + "!";
 
         ObjectFactory factory = new ObjectFactory();
         Greeting response = factory.createGreeting();
